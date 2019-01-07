6edb3b878436be30000bd92e2b57c9959ecae82e
Catsuri33 <Catsuri.YT@gmail.com>
2019-01-07 18:22:22 +0100

    Update MainGameLoop.java

--- src/fr/catsuri33/warifact/engineTester/MainGameLoop.java
+++ src/fr/catsuri33/warifact/engineTester/MainGameLoop.java
@@ -1,6 +1,10 @@
 package fr.catsuri33.warifact.engineTester;
 
 import fr.catsuri33.warifact.renderEngine.DisplayManager;
+import fr.catsuri33.warifact.renderEngine.Loader;
+import fr.catsuri33.warifact.renderEngine.RawModel;
+import fr.catsuri33.warifact.renderEngine.Renderer;
+import fr.catsuri33.warifact.shaders.StaticShader;
 import org.lwjgl.opengl.Display;
 
 public class MainGameLoop {
@@ -9,12 +13,37 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f
+        };
+
+        int[] indices = {
+                0,1,3,
+                3,1,2
+        };
+
+        RawModel model = loader.loadToVAO(vertices, indices);
+
         while(!Display.isCloseRequested()){
 
+            // game logic
+            renderer.prepare();
+            shader.start();
+            renderer.render(model);
+            shader.stop();
             DisplayManager.updateDisplay();
 
         }
 
+        shader.cleanUP();
+        loader.cleanUP();
         DisplayManager.closeDisplay();
 
     }
